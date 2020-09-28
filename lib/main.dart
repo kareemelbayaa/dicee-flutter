8a8e1e480cc4060c9eea8fcc8de79bce6eb6485a
Karim Elbayaa <kareemelbayaa@gmail.com>
2020-09-28 07:04:14 +0200

    complete module 04 of the dice rolling application in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('left button pressed');
+                  setState(() {
+                    //to build the ui again after the change
+                    leftDiceNumber = generateDiceNumber(leftDiceNumber);
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print('right button pressed');
+                  setState(() {
+                    rightDiceNumber = generateDiceNumber(rightDiceNumber);
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          )
+        ],
+      ),
+    );
+  }
+
+  int generateDiceNumber(int diceNumber) {
+    diceNumber = Random().nextInt(6)+1;
+    return diceNumber;
   }
 }
